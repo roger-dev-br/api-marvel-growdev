75ca6f20c89e51592e57b1857df32793fe7fa053
Roger Medeiros <rogermedeiros@outlook.com>
2021-01-19 16:53:24 -0300

    ignore

--- dist/app/middlewares/logRequests.js
+++
@@ -1,8 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});exports. default = (req, res, next) => {
-    const { method, url } = req;
-
-    const logLabel = `[${method.toUpperCase()} ${url}]`;
-    console.log(logLabel);
-
-    return next();
-};
