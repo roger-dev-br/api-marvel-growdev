3d304d2e9b98c6e3d9c78fe702f3fcff9c01438f
Roger Medeiros <rogermedeiros@outlook.com>
2021-01-26 19:06:26 -0300

    ajuste no show

--- src/app/service/marvelController.js
+++ src/app/service/marvelController.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 class MarvelController {
     constructor() {
-        this.urlBase = `https://gateway.marvel.com/v1/public/characters?apikey=8b0223911c50629c4a9c4c76d96105f4&hash=eb74672f8bf6d58342c6c006109a2435&ts=1609890812920`;
+        this.urlBase = `https://gateway.marvel.com/v1/public/`;
+        this.autenticacao = `?apikey=8b0223911c50629c4a9c4c76d96105f4&hash=eb74672f8bf6d58342c6c006109a2435&ts=1609890812920`;
         this.registrosPorPagina = 100;
     }
 
@@ -16,7 +17,7 @@ class MarvelController {
             offset = ((pagina - 1) * this.registrosPorPagina) + 1;
         }
 
-        let url = this.urlBase + `&limit=${this.registrosPorPagina}&offset=${offset}`;
+        let url = `${this.urlBase}/characters${this.autenticacao}&limit=${this.registrosPorPagina}&offset=${offset}`;
 
         if (nome) {
             url += `&nameStartsWith=${nome}`;
@@ -31,22 +32,9 @@ class MarvelController {
     show = (req, res) => {
         const { id } = req.params;
 
-        /// requsicao para a API da marvel com o id;
-        const personagem =       {
-            id: 1010697,
-            name: "Virginia Dare",
-            description: "",
-            modified: "1969-12-31T19:00:00-0500",
-            thumbnail: {
-                path: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available",
-                extension: "jpg"
-            },
-            comics: {
-                items: [],
-            }
-        };
-       
-        res.json({ personagem });
+        axios.get(`${this.urlBase}/characters/${id}${this.autenticacao}`).then(resposta => {
+            return res.json(resposta.data.data);
+        });
     }
 }
 
